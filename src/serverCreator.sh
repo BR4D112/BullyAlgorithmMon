1bfa675b34cef0addace3b724ba4fc86079bd148
darckronoz <70179923+darckronoz@users.noreply.github.com>
2024-05-15 08:51:19 -0500

    Update serverCreator.sh

--- src/serverCreator.sh
+++ src/serverCreator.sh
@@ -6,7 +6,8 @@ id=$2
 puertos_conocidos=$3
 
 # Crear y ejecutar el contenedor
-docker run -d -p $puerto_disponible:3001 -e SERVER_ID=$id -e MY_WEIGHT=10 -e MY_PORT=$puerto_disponible -e KNOWN_PORTS=$puertos_conocidos --name "server$puerto_disponible" lonyonserver
+# elimino el wieght porque se usa unicmaente el id (y)
+docker run -d -p $puerto_disponible:3001 -e SERVER_ID=$id -e MY_PORT=$puerto_disponible -e KNOWN_PORTS=$puertos_conocidos --name "server$puerto_disponible" lonyonserver
 
 # Mostrar mensaje informativo
 echo "Servidor creado en el puerto $puerto_disponible, con el nombre 'server$puerto_disponible' y con el id $id., y la lista de puertos $puertos_conocidos"
